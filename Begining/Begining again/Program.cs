d7ac5c92319108b279d52569cf69b0890349f6c7
LosCrimson <lukas.suslavicius@gmail.com>
2022-11-09 20:16:41 +0200

    Add project files.

---
+++ Begining/Begining again/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begining_again
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
